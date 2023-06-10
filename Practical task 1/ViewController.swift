211e3ba7ab547c7338e5cd3edfe4c494f005422e
aivanov <alex130896@gmail.com>
2023-06-10 21:30:01 +0400

    Initial Commit

---
+++ Practical task 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practical task 1
+//
+//  Created by Александр Иванов on 10.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
