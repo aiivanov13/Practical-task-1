36d9b06691e92b30b5a759cc1aa3553969122745
aivanov <alex130896@gmail.com>
2023-06-17 11:17:26 +0400

    Add background image
    Добавил фоновое изображение

--- Practical task 1/Sources/ViewController.swift
+++ Practical task 1/Sources/ViewController.swift
@@ -9,7 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - UI Elements
+
+    private lazy var backgroundImageView: UIImageView = {
+        let image = UIImage(named: "backgroundImage")
+        let imageView = UIImageView(image: image)
+        imageView.contentMode = .scaleAspectFill
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+
+        return imageView
+    }()
+
+    // MARK: - Life Cycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupHierarchy()
+        setupLayout()
+    }
+
+    // MARK: - Setups
+
+    private func setupHierarchy() {
+        view.addSubview(backgroundImageView)
+    }
+
+    private func setupLayout() {
+        NSLayoutConstraint.activate([
+            backgroundImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 139),
+            backgroundImageView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 88),
+            backgroundImageView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -87)
+        ])
     }
 }
