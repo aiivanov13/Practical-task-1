961578a6d631ccdcbdc2af88c82203022eed89a9
aivanov <alex130896@gmail.com>
2023-07-01 14:22:34 +0400

    Add navigation
    Добавил navigation controller

--- Practical task 1/App/AppDelegate.swift
+++ Practical task 1/App/AppDelegate.swift
@@ -12,7 +12,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        window?.rootViewController = ViewController()
+        let viewController = ViewController()
+        let navigationController = UINavigationController(rootViewController: viewController)
+        window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
 
         return true
