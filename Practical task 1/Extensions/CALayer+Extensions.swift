cadb46bba050f2022a155224437c93bcfb6d35a5
aivanov <alex130896@gmail.com>
2023-07-03 13:31:03 +0400

    Corrections
    Небольшие правки

--- Practical task 1/Extensions/CALayer+Extensions.swift
+++ Practical task 1/Extensions/CALayer+Extensions.swift
@@ -8,10 +8,12 @@
 import UIKit
 
 extension CALayer {
-    func addShadows() {
-        self.shadowColor = UIColor.black.cgColor
+    func addShadows(color: CGColor? = UIColor.black.cgColor) {
+        self.shadowColor = color
         self.shadowOpacity = 0.15
         self.shadowOffset = CGSize(width: 0, height: 12)
         self.shadowRadius = 36
+        self.shouldRasterize = true
+        self.rasterizationScale = UIScreen.main.scale
     }
 }
